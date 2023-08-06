ab4fe71b60f6f992ca0258cdac74e6fbfbce8a99
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2023-08-06 18:07:02 -0500

    creacion y solucion del punto 3 del taller 2

---
+++ taller_2/punto_3/src/App.java
@@ -0,0 +1,14 @@
+/**
+3. Calculadora Simple:
+Crea una clase Calculadora que contenga métodos para realizar operaciones matemáticas básicas,
+como suma, resta, multiplicación y división.
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Sumar 1 + 2: " + Calculadora.sumar(1, 2));
+        System.out.println("Restar 1 - 2: " + Calculadora.restar(1, 2));
+        System.out.println("Multiplicar 1 * 2: " + Calculadora.multiplicar(1, 2));
+        System.out.println("Dividir 1 / 2: " + Calculadora.dividir(1, 2));
+    }
+}
---
+++ taller_2/punto_3/src/Calculadora.java
@@ -0,0 +1,18 @@
+public class Calculadora {
+    
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public static double dividir(double a, double b) {
+        return a / b;
+    }
+}
