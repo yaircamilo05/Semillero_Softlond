8012826a3eb0301c9c1bed5ba8fbfe033c664a23
Yair Sanchez <yair.sanchez21369@ucaldas.edu.co>
2023-08-06 18:13:19 -0500

    creacion de las clases del punto 4 del taller 2

---
+++ taller_2/punto_4/src/App.java
@@ -0,0 +1,16 @@
+/**
+4. Sistema de Reservas de Hotel:
+Crear un sistema de reservas de hotel que permita a los clientes corporativos reservar múltiples habitaciones
+en diferentes hoteles para hospedar a su personal.
+Debes tener clases como Hotel, Habitación, Cliente, y Reserva. 
+Implementa métodos para listar habitaciones disponibles, realizar reservas y cancelar reservas. 
+Al final de la ejecución del programa debe mostrar la siguiente info:
+Nombre de la empresa que reservó
+Nombre y cédula de quien ocupará cada habitación y número de la habitación que ocupará. Esto para cada hotel.
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
